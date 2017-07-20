1d17bd514efc1e3c37b491c6501526e8e0112d50
1501132 <1501132@PC63419.asost.local>
2017-07-20 14:01:48 +0900

    変更

--- src/servlet/T01votetop.java
+++ src/servlet/T01votetop.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,7 +29,11 @@ public class T01votetop extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/T01Login.jsp");
+		rd.forward(request, response);
+		
 	}
 
 	/**
@@ -36,6 +42,9 @@ public class T01votetop extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/T01Login.jsp");
+		rd.forward(request, response);
 	}
 
 }
